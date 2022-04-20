c1816ee25c336f9023bc16ee47f6565bcf8743f9
kaibrown4 <98724196+kaibrown4@users.noreply.github.com>
2022-04-19 23:30:34 -0400

    Update raycast pixel.lua

--- raycast pixel.lua
+++ raycast pixel.lua
@@ -9,6 +9,17 @@ for x = 1, x_iter do
         local pixel = Instance.new("SpawnLocation", script)
         pixel.Size = Vector3.new(1,1,1)
         pixel.Position = Vector3.new(x, 3, z) + pos
+        pixel.Anchored = true
         Pixels[x][z] = pixel
+    end
+end
+
+while task.wait()*10 do
+    for x = 1, #Pixels do
+        for z = 1,#Pixels[x] do
+            local pixel = Pixels[x][z]
+            local ray = workspace:Raycast(pixel.Position, Pixel.Position - Vector3.new(0, 100, 0))
+            pixel.Color = ray.Instance.Color
+        end
     end
 end
