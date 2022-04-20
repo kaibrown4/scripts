4af42418581e98dcfbf0ae162c674cdc2c4746e0
kaibrown4 <98724196+kaibrown4@users.noreply.github.com>
2022-04-19 23:49:45 -0400

    Update raycast pixel.lua

--- raycast pixel.lua
+++ raycast pixel.lua
@@ -2,26 +2,27 @@ local x_iter = 30
 local z_iter = 30
 local pos = owner.Character.HumanoidRootPart.Position
 local Pixels = {}
+local t = 0
+local index = 1
  
 for x = 1, x_iter do
-    Pixels[x] = {}
     for z = 1, z_iter do
+        t += 1
         local pixel = Instance.new("SpawnLocation", script)
         pixel.Enabled = false
         pixel.Size = Vector3.new(1,1,1)
         pixel.Position = Vector3.new(x, 5, z) + pos
         pixel.Anchored = true
-        Pixels[x][z] = pixel
+        Pixels[t] = pixel
     end
 end
 
 while task.wait()*10 do
-    for x = 1, #Pixels do
-        for z = 1,#Pixels[x] do
-            local pixel = Pixels[x][z]
-            if pixel.Color ~= workspace:Raycast(pixel.Position, pixel.Position - Vector3.new(0, 100, 0)).Instance.Color then
-                pixel.Color = workspace:Raycast(pixel.Position, pixel.Position - Vector3.new(0, 100, 0)).Instance.Color
-            end
+    for i = 1,#Pixels do
+        local pixel = Pixels[index]
+        local ray = workspace:Raycast(pixel.Position, pixel.Position - Vector3.new(0, 100, 0))
+        if pixel.Color == ray.Instance.Color then
+            index += 1
         end
     end
 end
